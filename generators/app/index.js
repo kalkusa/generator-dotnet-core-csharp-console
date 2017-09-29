7b001c9ce0faab8073e8bf25f50c8d798abd9a37
kalkusa <kalkus@10g.pl>
2017-09-29 14:37:11 +0200

    Added .NET core console app template.

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,16 +1,52 @@
 var Generator = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
 
 module.exports = class extends Generator {
-    // The name `constructor` is important here
     constructor(args, opts) {
-      // Calling the super constructor is important so our generator is correctly set up
       super(args, opts);
-  
-      // Next, add your custom code
-      this.option('babel'); // This method adds support for a `--babel` flag
     }
 
-    init() {
-        this.log('foo :)');
+    // init() {
+    //     this.log('foo :)');
+    //     this.log(yosay('Welcome to the marvellous ' + chalk.red('C#') + ' generator!'));
+    // }
+    
+    prompting() {
+        return this.prompt([{
+          type    : 'input',
+          name    : 'name',
+          message : 'Your project name',
+          default : this.appname // Default to current folder name
+        }]).then((answers) => {
+          //this.log('app name', answers.name);
+          this.applicationName = answers.name;
+        });
     }
+
+    writing() {
+        this.fs.copyTpl(
+          this.templatePath('Program.cs'),
+          this.destinationPath('Program.cs'),
+          { applicationName: this.applicationName }
+        );
+
+        this.fs.copyTpl(
+            this.templatePath('project.csproj'),
+            this.destinationPath(this.applicationName + '.csproj'),
+            { applicationName: this.applicationName }
+        );
+
+        this.fs.copyTpl(
+            this.templatePath('.vscode/launch.json'),
+            this.destinationPath('.vscode/launch.json'),
+            { applicationName: this.applicationName }
+        );
+
+        this.fs.copyTpl(
+            this.templatePath('.vscode/tasks.json'),
+            this.destinationPath('.vscode/tasks.json'),
+            { applicationName: this.applicationName }
+        );
+      }
 };
---
+++ generators/app/templates/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace <%= applicationName %>
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
